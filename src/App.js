7dc220caa16a02ab60456e30e0bde1e9ad415af8
mn2337 <mn2337@nyu.edu>
2019-12-05 14:42:15 -0500

    framework done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
 
+import Header from './components/Header';
 import Login from './pages/Login';
 import UserProfile from './pages/UserProfile';
 import Signup from './pages/Signup';
@@ -8,8 +10,9 @@ import Logout from './pages/Logout';
 
 function App() {
   return (
-  	
+
     <div className="App">
+    	<Header loggedIn={true} />
     	<Router>
 			<Route exact path="/" component={UserProfile} />
     		<Route exact path="/log-in" component={Login} />
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header({ loggedIn }) {
+	return (
+		<header classname="Head">
+			<nav>
+				{loggedIn &&<a href="/"></a>}
+				{!loggedIn &&<a href="/login">Login</a>}
+				{!loggedIn &&<a href="/sign-up">Sign Up</a>}
+				{loggedIn &&<a href="/log-out">Log out</a>}
+			</nav>
+		</header>
+	);
+}
